d0f8713bbff5eb7304c6e945ae0317bb5109f286
ebukathedev <ebuskelele@gmail.com>
2023-09-08 11:29:39 +0100

    update utc time

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const currentDayOfWeek = document.querySelector("#today");
-const utcTime = document.querySelector("#utc-time");
+const utcTimeDisplay = document.querySelector("#utc-time");
 
 const date = new Date();
 const weekDay = date.toLocaleDateString("en-US", { weekday: "long" });
-const utc = date.getTime();
+
+const showTime = () => {
+	const date = new Date();
+	let utcHours = date.getUTCHours();
+	let utcMinutes = date.getUTCMinutes();
+	let utcSeconds = date.getUTCSeconds();
+	let utcTime;
+
+	utcHours = utcHours < 10 ? `0${utcHours}` : utcHours;
+	utcMinutes = utcMinutes < 10 ? `0${utcMinutes}` : utcMinutes;
+	utcSeconds = utcSeconds < 10 ? `0${utcSeconds}` : utcSeconds;
+
+	utcTime = `${utcHours}:${utcMinutes}:${utcSeconds}`;
+
+	utcTimeDisplay.textContent = utcTime;
+	setTimeout(showTime, 1000);
+};
+showTime();
 
 currentDayOfWeek.textContent = weekDay;
-utcTime.textContent = utc;
